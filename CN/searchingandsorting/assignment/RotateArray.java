1a69235e6f0a61e0bab43365f1ec26e2c24ec522
hemantmudgal <hemant.mudgal_cs19@gla.ac.in>
2022-04-14 14:21:05 +0530

    First Method for rotateArray

---
+++ CN/searchingandsorting/assignment/RotateArray.java
@@ -0,0 +1,30 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Hemant Mudgal
+ *   Date: 13-04-2022
+ *   Time: 21:20
+ *   File: RotateArray.java
+ */
+
+package CN.searchingandsorting.assignment;
+
+import java.util.Arrays;
+
+public class RotateArray {
+   public static void rotateArray(int[] array, int index){
+       for(int i = 0 ; i < index; i++){
+           int temp = array[0];
+           for(int j = 0; j <array.length-1;j++){
+
+               array[j] = array[j+1];
+           }
+           array[array.length-1] = temp;
+       }
+   }
+
+    public static void main(String[] args){
+        int[] array = {1,2,3,4,5,6,7};
+        int index = 4;
+        rotateArray(array, index);
+        System.out.println(Arrays.toString(array));
+    }
+}
