92f0cad5a0efdd47b04dcf4a313da4640210b921
hemantmudgal <hemant.mudgal_cs19@gla.ac.in>
2022-04-18 14:20:50 +0530

    Maximum sub array sum

---
+++ DSA9_10/MaximumSubArraySum.java
@@ -0,0 +1,23 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Hemant Mudgal
+ *   Date: 18-04-2022
+ *   Time: 14:20
+ *   File: MaximumSubArraySum.java
+ */
+
+package DSA9_10;
+
+public class MaximumSubArraySum {
+    public static long maxSubarraySum(int[] arr, int n) {
+        long sum = (long)0;
+        long max = Long.MIN_VALUE;
+
+        for(int i = 0; i< arr.length; i++){
+            sum += arr[i];
+            if(sum < 0)
+                sum = 0;
+            max = Math.max(sum, max);
+        }
+        return max;
+    }
+}
