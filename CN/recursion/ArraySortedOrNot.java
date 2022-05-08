296d0498815328d986b6502362fad693b0337d0f
hemantmudgal <hemant.mudgal_cs19@gla.ac.in>
2022-05-08 18:04:04 +0530

    check Array Sorted

---
+++ CN/recursion/ArraySortedOrNot.java
@@ -0,0 +1,30 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Hemant Mudgal
+ *   Date: 08-05-2022
+ *   Time: 17:53
+ *   File: ArraySortedOrNot.java
+ */
+
+package CN.recursion;
+
+public class ArraySortedOrNot {
+    public static boolean checkSorted(int[] array){
+        if(array.length <= 1){
+            return true;
+        }
+        if(array[0] > array[1]){
+            return false;
+        }
+        int[] smallInput = new int[array.length - 1];
+        for (int i = 1; i < array.length; i++) {
+            smallInput[i-1] = array[i];
+        }
+        boolean smallAns = checkSorted(smallInput);
+        return smallAns;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {2,1,2,3,4,5};
+        System.out.println(checkSorted(array));
+    }
+}
