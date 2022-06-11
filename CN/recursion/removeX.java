947f5868374ef644dbe050e464d12419f66346ef
hemantmudgal <hemant.mudgal_cs19@gla.ac.in>
2022-06-11 23:30:22 +0530

    remove all x recursively in a string

---
+++ CN/recursion/removeX.java
@@ -0,0 +1,30 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Hemant Mudgal
+ *   Date: 11-06-2022
+ *   Time: 23:23
+ *   File: removeX.java
+ */
+
+package CN.recursion;
+
+public class removeX {
+
+    public static String removeX(String str){
+        String mainString;
+        if(str.length() == 0){
+            return"";
+        }
+
+        String smallAns = removeX(str.substring(1));
+        if(str.charAt(0) == 'x'){
+            mainString = smallAns;
+        }else{
+            mainString = str.charAt(0) + smallAns;
+        }
+        return mainString;
+    }
+    public static void main(String[] args){
+        String str = "xaxb";
+        System.out.println(removeX(str));;
+    }
+}
