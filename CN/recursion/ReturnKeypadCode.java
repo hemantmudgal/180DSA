aff2832071fdd98eaddb3edbb2e8522e3d6c64a7
hemantmudgal <hemant.mudgal_cs19@gla.ac.in>
2022-06-13 12:13:31 +0530

    return keypad code

---
+++ CN/recursion/ReturnKeypadCode.java
@@ -0,0 +1,65 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Hemant Mudgal
+ *   Date: 13-06-2022
+ *   Time: 10:00
+ *   File: ReturnKeypadCode.java
+ */
+
+package CN.recursion;
+
+import java.util.Arrays;
+
+public class ReturnKeypadCode {
+
+    public static String[] findKeypadSubSequences(int value) {
+        String[] ans = {""};
+        if(value == 0){
+            return ans;
+        }
+
+        String[] smallAns = findKeypadSubSequences(value/10);
+        String lastCharacter = getString(value % 10);
+        ans = new String[smallAns.length * lastCharacter.length()];
+        int k = 0;
+        for (int i = 0; i < lastCharacter.length(); i++) {
+            for (int j = 0; j < smallAns.length; j++) {
+                ans[k] = smallAns[j] + lastCharacter.charAt(i);
+                k++;
+            }
+        }
+        return ans;
+
+    }
+
+    public static String getString(int input){
+        if(input == 2){
+            return "abc";
+        }
+        if(input == 3){
+            return "def";
+        }
+        if(input == 4){
+            return "ghi";
+        }
+        if(input == 5){
+            return "jkl";
+        }
+        if(input == 6){
+            return "mno";
+        }
+        if(input == 7){
+            return "pqr";
+        }if(input == 8){
+            return "ust";
+        }if(input == 9){
+            return "wxyz";
+        }
+        return " ";
+    }
+
+    public static void main(String[] args) {
+        int value = 23;
+        String[] array = findKeypadSubSequences(value);
+        System.out.println(Arrays.toString(array));
+    }
+}
