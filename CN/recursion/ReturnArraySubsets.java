4c7a2269621ad0805f46b27110ac3d9192ce9c97
hemantmudgal <hemant.mudgal_cs19@gla.ac.in>
2022-06-24 11:54:46 +0530

    return array subsets

--- CN/recursion/ReturnArraySubsets.java
+++ CN/recursion/ReturnArraySubsets.java
@@ -13,22 +13,23 @@ public class ReturnArraySubsets {
             int[][] output = new int[1][0];
             return output;
         }
+        int[][] smallAns = findAllSubsets(array, index + 1);
+        int[][] output = new int[smallAns.length * 2][];
 
-        int[][] smallOutput = findAllSubsets(array, index + 1);
-        int[][] output = new int[smallOutput.length * 2][];
         int k = 0;
-        for (int i = 0; i < smallOutput.length; i++) {
-            output[k] = new int[smallOutput[i].length];
-            for (int j = 0; j < smallOutput[i].length; j++) {
-                output[k][j] = smallOutput[i][j];
+        for (int i = 0; i < smallAns.length; i++) {
+            output[k] =  new int[smallAns[i].length];
+            for (int j = 0; j < smallAns[i].length; j++) {
+                output[k][j] = smallAns[i][j];
             }
             k++;
         }
-        for (int i = 0; i < smallOutput.length; i++) {
-           output[k] = new int[smallOutput[i].length + 1];
+
+        for (int i = 0; i < smallAns.length; i++) {
+            output[k] = new int[smallAns[i].length + 1];
             output[k][0] = array[index];
-            for (int j = 1; j <= smallOutput[i].length; j++) {
-                output[k][j] = smallOutput[i][j-1];
+            for (int j = 1; j <= smallAns[i].length; j++) {
+                output[k][j] = smallAns[i][j-1];
             }
             k++;
         }
@@ -39,9 +40,10 @@ public class ReturnArraySubsets {
         int[] array = {15,20,12};
         int[][] output = findAllSubsets(array,0);
         for (int i = 0; i < output.length; i++) {
-            for (int j = 0; j < output[0].length; j++) {
-                System.out.println(output[i][j]);
+            for (int j = 0; j < output[i].length; j++) {
+                System.out.print(output[i][j] + " ");
             }
+            System.out.println();
         }
     }
 }
